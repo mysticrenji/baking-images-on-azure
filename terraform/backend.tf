274f4aaaa241a9523a0eef26a4e5df2b20623290
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2023-04-13 12:15:49 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "golden-image-factory-backend"
-    storage_account_name = "terraformbackendfactory"
+    storage_account_name = "terraformbackendsfactory"
     container_name       = "terraformstate"
     key                  = "terraform.tfstate"
 
